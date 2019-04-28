45e39cd30a83092eb1216ef648e58f78fd51d030
nyx-ell <nicole.linyx@gmail.com>
2019-04-28 15:33:40 +0800

    added jwt

---
+++ helpers.py
@@ -0,0 +1,27 @@
+import jwt
+import datetime
+from app import app
+
+def encode_auth_token(self):
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': self.id
+        }
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        ).decode('utf-8')
+    except Exception as e:
+        return e
+
+def decode_auth_token(auth_token):
+    try:
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
--- journal_api/blueprints/users/views.py
+++ journal_api/blueprints/users/views.py
@@ -1,9 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+import jwt
+from models.user import User
+from werkzeug.security import generate_password_hash
+from helpers import encode_auth_token, decode_auth_token
 
 users_api_blueprint = Blueprint('users_api',
                              __name__,
                              template_folder='templates')
 
-@users_api_blueprint.route('/', methods=['GET'])
-def index():
+@users_api_blueprint.route('/', methods=['POST'])
+def create():
+    req_data = request.get_json()
+    first_name = req_data['firstName']
+    last_name = req_data['lastName']
+    email = req_data['email']
+    hashed_password = generate_password_hash(req_data['password'])
+
     return "USERS API"
