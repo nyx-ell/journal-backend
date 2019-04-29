e8840f020806424ed59279c1d8e7111adb9555c4
nyx-ell <nicole.linyx@gmail.com>
2019-04-29 11:31:08 +0800

    updated create user endpoint

--- journal_api/blueprints/users/views.py
+++ journal_api/blueprints/users/views.py
@@ -8,7 +8,7 @@ users_api_blueprint = Blueprint('users_api',
                              __name__,
                              template_folder='templates')
 
-@users_api_blueprint.route('/register', methods=['POST'])
+@users_api_blueprint.route('/new', methods=['POST'])
 def create():
     req_data = request.get_json()
     first_name = req_data['firstName']
