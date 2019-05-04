d805870a364cd8bfa74b920fdbc65867946b5847
nyx-ell <nicole.linyx@gmail.com>
2019-05-04 22:19:58 +0800

    added s3

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import jwt
 import datetime
 from app import app
+import boto3, botocore
 
 def encode_auth_token(self):
     try:
@@ -25,3 +26,33 @@ def decode_auth_token(auth_token):
         return 'Signature expired. Please log in again.'
     except jwt.InvalidTokenError:
         return 'Invalid token. Please log in again.'
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=app.config['S3_KEY'],
+   aws_secret_access_key=app.config['S3_SECRET']
+)
+
+def upload_file_to_s3(file, bucket_name, acl="public-read"):
+    user = current_user
+    try:
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
+
+    return f"{file.filename}"
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ['png', 'jpg', 'jpeg', 'gif']
